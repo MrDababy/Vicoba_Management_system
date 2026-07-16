c0751e4e44faded75d8f7918d0df7f043aff1404
MrDababy <kennymutimagwe@gmail.com>
2026-07-16 14:51:02 +0300

    minor changes in the base url and app url to match the project name

--- config/config.php
+++ config/config.php
@@ -13,9 +13,9 @@
 // Application Settings
 define('APP_NAME', 'VICOBA Management System');
 define('APP_VERSION', '1.0.0');
-define('APP_URL', 'http://100.59.187.91');
+define('APP_URL', 'http://100.59.187.91/Vicoba_Management_system');
 define('APP_ENV', 'development'); // development | production | testing
-define('BASE_URL', '');
+define('BASE_URL', '/Vicoba_Management_system/public');
 
 // Timezone Settings
 date_default_timezone_set('Africa/Dar_es_Salaam');
